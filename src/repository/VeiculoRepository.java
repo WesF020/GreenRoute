6f4ae5ea68d0fa3bc2e821a605359c39012744a9
WesF020 <wesleyzyarno020@gmail.com>
2026-06-07 06:14:05 -0300

    feat: adiciona classe VeiculoRepository ao pacote repository.

---
+++ src/repository/VeiculoRepository.java
@@ -0,0 +1,54 @@
+package repository;
+import model.Veiculo;
+
+public class VeiculoRepository {
+    private Veiculo[] veiculos = new Veiculo[10];
+    private int quantidadeVeiculosCadastrados = 0;
+    private int proximoId = 0;
+
+    // CRUD: Cadastro de Veículos:
+    public void cadastrarVeiculo(Veiculo veiculo){
+        veiculo.setId(proximoId++);
+        veiculos[quantidadeVeiculosCadastrados] = veiculo;
+        quantidadeVeiculosCadastrados++;
+    }
+    // Crud: Procurando Id do Veículo:
+    public Veiculo buscarPorId(int id) {
+        for (int i = 0; i < quantidadeVeiculosCadastrados; i++) {
+            if (veiculos[i].getId() == id) {
+                return veiculos[i];
+            }
+        }
+        return null;
+    }
+    // CRUD: Listar todos os veículos:
+    public Veiculo[] listarTodosVeiculos(){
+        return veiculos;
+    }
+
+    // CRUD: Atualizar Veículo:
+    public boolean atualizarVeiculo(int id, Veiculo veiculoAtualizado){
+        for (int i = 0; i < quantidadeVeiculosCadastrados; i++){
+            if (veiculos[i].getId() == id){
+                veiculos[i] = veiculoAtualizado;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // CRUD: Apagar Veículo:
+    public boolean apagarVeiculo(int id){
+        for (int i = 0; i < quantidadeVeiculosCadastrados; i++){
+            if (veiculos[i].getId() == id){
+                for (int j = i; j < quantidadeVeiculosCadastrados - 1; j++){
+                    veiculos[j] = veiculos[j + 1];
+                }
+                veiculos[quantidadeVeiculosCadastrados - 1] = null;
+                quantidadeVeiculosCadastrados--;
+                return true;
+            }
+        }
+        return false;
+    }
+}
