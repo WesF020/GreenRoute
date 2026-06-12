8b3126f3edb40a0de15c6d2f0c524ab95792c40f
phumberto <phmedeiroslima@gmail.com>
2026-06-12 14:58:38 -0300

    feat: Adiciona a classe buscarEletropostoPorIdController em EletropostoController

--- src/controller/EletropostoController.java
+++ src/controller/EletropostoController.java
@@ -7,21 +7,25 @@ public class EletropostoController {
     private EletropostoRepository eletropostoRepository;
 
     // Construtor
-    public EletropostoController(EletropostoRepository eletropostoRepository){
-        this.eletropostoRepository = eletropostoRepository;
+    public EletropostoController(EletropostoRepository eletropostoRepository){this.eletropostoRepository = eletropostoRepository;
     }
+
     // CRUD: Criar (Create)
-    public void cadastrarEletropostoController(Eletroposto eletroposto){
-        eletropostoRepository.cadastrarEletroposto(eletroposto);
+    public void cadastrarEletropostoController(Eletroposto eletroposto){eletropostoRepository.cadastrarEletroposto(eletroposto);
     }
+
     // CRUD: Ler (Read)
     public Eletroposto[] listarEletropostoController(){
         return eletropostoRepository.listarEletropostos();
     }
+
+    //CRUD: Buscar por ID:
+    public Eletroposto buscarEletropostoPorIdController(int id) {return eletropostoRepository.buscarPorId(id);}
+
     // CRUD: Atualizar (Update)
-    public boolean atualizarEletropostoController(int id, Eletroposto eletropostoAtualizado){
-        return eletropostoRepository.atualizarEletroposto(id, eletropostoAtualizado);
+    public boolean atualizarEletropostoController(int id, Eletroposto eletropostoAtualizado){return eletropostoRepository.atualizarEletroposto(id, eletropostoAtualizado);
     }
+
     // CRUD: Apagar (Apagar)
     public boolean apagarEletropostoController(int id){
         return eletropostoRepository.apagarEletroposto(id);
