613999736594cf9ab1e522b942da76fe42d59c06
phumberto <phmedeiroslima@gmail.com>
2026-06-14 15:28:25 -0300

    feat: Adiciona o metodo buscarCidadePorIdController em CidadeController, chamado pelo VeiculoController em simularRota.

--- src/controller/CidadeController.java
+++ src/controller/CidadeController.java
@@ -1,4 +1,5 @@
 package controller;
+
 import model.Cidade;
 import repository.CidadeRepository;
 
@@ -6,29 +7,33 @@ public class CidadeController {
     private CidadeRepository cidadeRepository;
 
     // Construtor
-    public CidadeController(CidadeRepository cidadeRepository){
+    public CidadeController(CidadeRepository cidadeRepository) {
         this.cidadeRepository = cidadeRepository;
     }
 
     // CRUD: Cadastrar Cidade (Create)
-    public void cadastrarCidadeController(Cidade cidade){
+    public void cadastrarCidadeController(Cidade cidade) {
         cidadeRepository.cadastrarCidade(cidade);
     }
 
     // CRUD: Listar Cidades (Read)
-    public Cidade[] listarCidadesController (){
-        return cidadeRepository.listarCidades();
+    public Cidade[] listarCidadesController() {
+        return cidadeRepository.listarTodasCidades();
     }
 
-    // CRUD: Buscar por ID:
-    public Cidade buscarCidadePorIdController(int id){return cidadeRepository.buscarPorId(id);}
+    // CRUD: Buscar Cidade por ID (Read)
+    // Usado pelo VeiculoController na simulação de rota
+    public Cidade buscarCidadePorIdController(int id) {
+        return cidadeRepository.buscarPorId(id);
+    }
 
     // CRUD: Atualizar Cidade (Update)
-    public boolean atualizarCidadeController(int id, Cidade cidadeAtualizada){
+    public boolean atualizarCidadeController(int id, Cidade cidadeAtualizada) {
         return cidadeRepository.atualizarCidade(id, cidadeAtualizada);
     }
+
     // CRUD: Apagar Cidade (Delete)
-    public boolean apagarCidadeController(int id){
+    public boolean apagarCidadeController(int id) {
         return cidadeRepository.apagarCidade(id);
     }
 }
