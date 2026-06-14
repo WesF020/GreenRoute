313ea64532d49cc77d63a0e49207b0988b64ca59
phumberto <phmedeiroslima@gmail.com>
2026-06-14 15:29:43 -0300

    feat: Adicionada a classe buscarEletropostosPorCidadeController. Que e chamada por VeiculoController quando a autonomia e insuficiente.

--- src/controller/EletropostoController.java
+++ src/controller/EletropostoController.java
@@ -1,34 +1,34 @@
 package controller;
+
 import model.Eletroposto;
 import repository.EletropostoRepository;
-
 
 public class EletropostoController {
     private EletropostoRepository eletropostoRepository;
 
     // Construtor
-    public EletropostoController(EletropostoRepository eletropostoRepository){this.eletropostoRepository = eletropostoRepository;
+    public EletropostoController(EletropostoRepository eletropostoRepository) {this.eletropostoRepository = eletropostoRepository;
     }
 
-    // CRUD: Criar (Create)
-    public void cadastrarEletropostoController(Eletroposto eletroposto){eletropostoRepository.cadastrarEletroposto(eletroposto);
+    // CRUD: Cadastrar Eletroposto (Create)
+    public void cadastrarEletropostoController(Eletroposto eletroposto) {eletropostoRepository.cadastrarEletroposto(eletroposto);
     }
 
-    // CRUD: Ler (Read)
-    public Eletroposto[] listarEletropostoController(){
+    // CRUD: Listar todos os Eletropostos (Read)
+    public Eletroposto[] listarEletropostoController() {
         return eletropostoRepository.listarEletropostos();
     }
 
-    //CRUD: Buscar por ID:
-    public Eletroposto buscarEletropostoPorIdController(int id) {return eletropostoRepository.buscarPorId(id);}
-
-    // CRUD: Atualizar (Update)
-    public boolean atualizarEletropostoController(int id, Eletroposto eletropostoAtualizado){return eletropostoRepository.atualizarEletroposto(id, eletropostoAtualizado);
+    // CRUD: Buscar Eletropostos por Cidade (Read)
+    public Eletroposto[] buscarEletropostosPorCidadeController(int cidadeId) {return eletropostoRepository.buscarPorCidadeId(cidadeId);
     }
 
-    // CRUD: Apagar (Apagar)
-    public boolean apagarEletropostoController(int id){
+    // CRUD: Atualizar Eletroposto (Update)
+    public boolean atualizarEletropostoController(int id, Eletroposto eletropostoAtualizado) {return eletropostoRepository.atualizarEletroposto(id, eletropostoAtualizado);
+    }
+
+    // CRUD: Apagar Eletroposto (Delete)
+    public boolean apagarEletropostoController(int id) {
         return eletropostoRepository.apagarEletroposto(id);
     }
-
 }
