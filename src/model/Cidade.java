bb6b78a2749cbf17ec608e554f8b4f5961906276
phumberto <phmedeiroslima@gmail.com>
2026-07-05 20:38:51 -0300

    refactor: Tratamento de erro agora com exception

--- src/model/Cidade.java
+++ src/model/Cidade.java
@@ -1,35 +1,36 @@
 package model;
 
+import exceptions.ValorInvalidoException;
+
 public class Cidade {
     private int id;
     private String nome;
     private String estado;
     private double distanciaDaCapital;
 
-    // Construtor
-    public Cidade(int id, String nome, String estado, double distanciaDaCapital){
+    public Cidade(int id, String nome, String estado, double distanciaDaCapital) {
         this.id = id;
         this.nome = nome;
         this.estado = estado;
-        this.distanciaDaCapital = distanciaDaCapital;
+        setDistanciaDaCapital(distanciaDaCapital);
     }
 
     // Getters
-    public int getId() {return id;}
-    public String getNome() {return nome;}
-    public String getEstado() {return estado;}
-    public double getDistanciaDaCapital() {return distanciaDaCapital;}
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public String getEstado() { return estado; }
+    public double getDistanciaDaCapital() { return distanciaDaCapital; }
 
     // Setters
-    public void setId(int id) {this.id = id;}
-    public void setNome(String nome) {this.nome = nome;}
-    public void setEstado(String estado) {this.estado = estado;}
-    public void setDistanciaDaCapital(double distanciaDaCapital){
-        if(distanciaDaCapital >= 0){
+    public void setId(int id) { this.id = id; }
+    public void setNome(String nome) { this.nome = nome; }
+    public void setEstado(String estado) { this.estado = estado; }
+
+    public void setDistanciaDaCapital(double distanciaDaCapital) {
+        if (distanciaDaCapital >= 0) {
             this.distanciaDaCapital = distanciaDaCapital;
         } else {
-            throw new IllegalArgumentException("Erro! A distância não pode ser menor que 0.");
+            throw new ValorInvalidoException("A distância não pode ser menor que 0.");
         }
     }
-
 }
--- src/model/Veiculo.java
+++ src/model/Veiculo.java
@@ -1,5 +1,7 @@
 package model;
 
+import exceptions.ValorInvalidoException;
+
 public abstract class Veiculo {
     private int id;
     private String modelo;
@@ -8,53 +10,56 @@ public abstract class Veiculo {
     private double consumoKwhPorKm;
     private int tempoRecargaCompleta;
 
-    public Veiculo(int id, String modelo, double autonomiaMaxima, double cargaBateriaAtual, double consumoKwhPorKm, int tempoRecargaCompleta){
+    public Veiculo(int id, String modelo, double autonomiaMaxima, double cargaBateriaAtual, double consumoKwhPorKm, int tempoRecargaCompleta) {
         this.id = id;
         this.modelo = modelo;
-        this.autonomiaMaxima = autonomiaMaxima;
-        this.cargaBateriaAtual = cargaBateriaAtual;
-        this.consumoKwhPorKm = consumoKwhPorKm;
-        this.tempoRecargaCompleta = tempoRecargaCompleta;
+        setAutonomiaMaxima(autonomiaMaxima);
+        setCargaBateriaAtual(cargaBateriaAtual);
+        setConsumoKwhPorKm(consumoKwhPorKm);
+        setTempoRecargaCompleta(tempoRecargaCompleta);
     }
 
     // Getters
-    public int getId() {return id;}
-    public String getModelo() {return modelo;}
-    public double getAutonomiaMaxima() {return autonomiaMaxima;}
-    public double getCargaBateriaAtual() {return cargaBateriaAtual;}
-    public double getConsumoKwhPorKm() {return consumoKwhPorKm;}
-    public int getTempoRecargaCompleta() {return tempoRecargaCompleta;}
+    public int getId() { return id; }
+    public String getModelo() { return modelo; }
+    public double getAutonomiaMaxima() { return autonomiaMaxima; }
+    public double getCargaBateriaAtual() { return cargaBateriaAtual; }
+    public double getConsumoKwhPorKm() { return consumoKwhPorKm; }
+    public int getTempoRecargaCompleta() { return tempoRecargaCompleta; }
 
     // Setters
-    public void setId(int id) {this.id = id;}
-    public void setModelo(String nome) {this.modelo = modelo;}
+    public void setId(int id) { this.id = id; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
     public void setAutonomiaMaxima(double autonomiaMaxima) {
-        if(autonomiaMaxima > 0){
+        if (autonomiaMaxima > 0) {
             this.autonomiaMaxima = autonomiaMaxima;
         } else {
-            throw new IllegalArgumentException("Erro! Autonomia não pode ser menor ou igual a 0.");
-        }
-    }
-    public void setCargaBateriaAtual(double cargaBateriaAtual) {
-        if(cargaBateriaAtual >= 0 && cargaBateriaAtual <= 100){
-            this.cargaBateriaAtual = cargaBateriaAtual;
-        } else {
-            throw new IllegalArgumentException("A carga da bateria deve estar entre 0 e 100.");
-        }
-    }
-    public void setConsumoKwhPorKm(double consumoKwhPorKm) {
-        if(consumoKwhPorKm > 0){
-            this.consumoKwhPorKm = consumoKwhPorKm;
-        } else {
-            throw new IllegalArgumentException("Erro! Consumo não pode ser menor ou igual a 0.");
-        }
-    }
-    public void setTempoRecargaCompleta(int tempoRecargaCompleta){
-        if(tempoRecargaCompleta >= 0){
-            this.tempoRecargaCompleta = tempoRecargaCompleta;
-        } else {
-            throw new IllegalArgumentException("Erro! Tempo de recarga não pode ser menor que 0s.");
+            throw new ValorInvalidoException("Autonomia não pode ser menor ou igual a 0.");
         }
     }
 
+    public void setCargaBateriaAtual(double cargaBateriaAtual) {
+        if (cargaBateriaAtual >= 0 && cargaBateriaAtual <= 100) {
+            this.cargaBateriaAtual = cargaBateriaAtual;
+        } else {
+            throw new ValorInvalidoException("A carga da bateria deve estar entre 0 e 100.");
+        }
+    }
+
+    public void setConsumoKwhPorKm(double consumoKwhPorKm) {
+        if (consumoKwhPorKm > 0) {
+            this.consumoKwhPorKm = consumoKwhPorKm;
+        } else {
+            throw new ValorInvalidoException("Consumo não pode ser menor ou igual a 0.");
+        }
+    }
+
+    public void setTempoRecargaCompleta(int tempoRecargaCompleta) {
+        if (tempoRecargaCompleta >= 0) {
+            this.tempoRecargaCompleta = tempoRecargaCompleta;
+        } else {
+            throw new ValorInvalidoException("Tempo de recarga não pode ser menor que 0.");
+        }
+    }
 }
