ba2a6bcdfb5f22dffc0d70682e80d69be795dc1b
phumberto <phmedeiroslima@gmail.com>
2026-07-05 14:37:02 -0300

    Adaptacao para ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-import controller.EletropostoController;
 import controller.CidadeController;
+import controller.EletropostoController;
+import controller.RotaController;
 import controller.VeiculoController;
 import repository.CidadeRepository;
 import repository.EletropostoRepository;
@@ -7,18 +8,23 @@ import repository.VeiculoRepository;
 import view.Menu;
 
 public class Main {
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
+        // Repositories
         VeiculoRepository veiculoRepository = new VeiculoRepository();
         CidadeRepository cidadeRepository = new CidadeRepository();
         EletropostoRepository eletropostoRepository = new EletropostoRepository();
 
-
+        // Controllers
+        VeiculoController veiculoController = new VeiculoController(veiculoRepository);
         CidadeController cidadeController = new CidadeController(cidadeRepository);
         EletropostoController eletropostoController = new EletropostoController(eletropostoRepository);
-        VeiculoController veiculoController = new VeiculoController(veiculoRepository, eletropostoController, cidadeController);
 
-        Menu menu = new Menu(veiculoController, eletropostoController, cidadeController);
+        // RotaController recebe os três controllers que precisa
+        RotaController rotaController = new RotaController(veiculoController, cidadeController, eletropostoController);
+
+        // View
+        Menu menu = new Menu(veiculoController, eletropostoController, cidadeController, rotaController);
         menu.exibirMenuPrincipal();
     }
 }
